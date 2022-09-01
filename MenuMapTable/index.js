a2bf67821c5eeef6c41f1591d8f1c51105db6b07
Maruf Bin Solaiman <0909maruf@gmail.com>
2022-09-01 14:22:33 +0600

    update

--- MenuMapTable/index.js
+++ MenuMapTable/index.js
@@ -38,19 +38,20 @@ menuDataPromise
         // displayCourseLabel(result);
         displayTable(result);
         console.log(result);
+        showParentMenu(result);
     })
     .catch((error) => {
         console.log(error);
     });
 
-$(`#menuForm`).on("submit", function (e) {
+$(`#menu_map_setup_form`).on("submit", function (e) {
     e.preventDefault();
     makeMenu();
 });
 
 function makeMenu() {
     // $(`#table-body`).empty();
-    let json = Object.fromEntries(new FormData($(`#menuForm`)[0]).entries());
+    let json = Object.fromEntries(new FormData($(`#menu_map_setup_form`)[0]).entries());
     $.ajax({
         type: "POST",
         url: "",
@@ -77,59 +78,125 @@ function displayTable(menus) {
     let tBody = $(`#table-body`);
     $.each(menus, (index, value) => {
         let row = $(`
-          <tr>
-            <td>${value.menuName}</td>
-            <td>${value.pageURL}</td>
+          <tr class="table-${value.isActive == 0 ? "danger" : "success"}">
+            <td class="text-left">${value.menuName}<span class="badge badge-${
+            value.isActive == 0 ? "danger" : "success"
+        } ml-3">${value.isActive == 0 ? "Inactive" : "Active"}</span></td>
+            <td class="text-left">${value.pageURL}</td>
             <td>${value.parentMenuTitle ? value.parentMenuTitle : "-"}</td>
             <td>${value.makePageTitle}</td>
             <td>${value.forLowerNavTitle}</td>
             <td>${value.isExternalLinkTitle}</td>
             <td>
-              <button class="btn btn-primary btn-sm">Toggle</button>
+              <button class="btn statusButton ${
+                  value.isActive == 0 ? "btn-success" : "btn-danger"
+              } btn-primary btn-sm">${
+            value.isActive == 0 ? "Activate" : "Deactivate"
+        }</button>
             </td>
             <td>
-              <button class="btn btn-primary btn-sm">Edit</button>
-              <button class="btn btn-primary btn-sm">Delete</button>
+              <button class="btn btn-secondary btn-sm editButton"><i class="fa-solid fa-pen"></i></button>
+              <button class="btn btn-danger btn-sm deleteButton" type="button"  ><i class="fa-solid fa-trash"></i></button>
             </td>
           </tr>
     
     `).appendTo(tBody);
 
-        let courseSelect = $(`.parentMenuSelect`, row);
-        $.each(courseLebels, (index, singleCourseLebel) => {
-            if (singleCourseLebel.course_label_no == value.course_label_no) {
-                courseSelect.append(`
-            <option selected value="${singleCourseLebel.course_label_no}">${singleCourseLebel.course_label_title}</option>
-            `);
-            } else {
-                courseSelect.append(`
-        <option value="${singleCourseLebel.course_label_no}">${singleCourseLebel.course_label_title}</option>
-        `);
-            }
+        let statusButton = $(`.statusButton`, row);
+        statusButton.on("click", function () {
+            console.log(value.isActive);
+            $.ajax({
+                type: "DELETE",
+                url: "",
+                data: value.isActive == "0" ? "1" : "0",
+                success: function (response) {
+                    console.log(value.isActive);
+                },
+            });
         });
 
-        let groupSelect = $(`.groupSelect`, row);
-        $.each(groups, (index, singleGroupSelect) => {
-            singleGroupSelect.group_no == value.group_no
-                ? groupSelect.append(`
-        <option selected value="${singleGroupSelect.group_no}">${singleGroupSelect.group_title}</option>
-        `)
-                : groupSelect.append(`
-        <option value="${singleGroupSelect.group_no}">${singleGroupSelect.group_title}</option>
-        `);
+        let editButton = $(`.editButton`, row);
+        editButton.on("click", function () {
+            // $(`#menu_map_setup_form`).empty();
+            $("#menu_map_setup_form")[0].reset();
+            $(`#exampleModalCenter`).modal("show");
+            $(`.modal-title`).text("Update Menu");
+            $(`#menuname`).val(value.menuName);
+            $(`#menuid`).val(value.menuId);
+            $(`#pageurl`).val(value.pageURL);
+
+            // $(`#parentmenu`).empty();
+            $(`#selectedParent`).text(value.parentMenuTitle);
         });
 
-        let sectionSelect = $(`.sectionSelect`, row);
-        $.each(section, (index, singleSectionSelect) => {
-            singleSectionSelect.section_no == value.section
-                ? sectionSelect.append(`
-              <option selected value="${singleSectionSelect.section_no}">${singleSectionSelect.section_title}</option>
-                `)
-                : sectionSelect.append(`
-                <option value="${singleSectionSelect.section_no}">${singleSectionSelect.section_title}</option>
-               `);
+        let deleteButton = $(`.deleteButton`, row);
+        deleteButton.on("click", function () {
+            $.ajax({
+                type: "DELETE",
+                url: "",
+                data: value.menuNo,
+                success: function (response) {
+                    console.log(value.menuNo);
+                },
+            });
         });
+
+        // $.each(courseLebels, (index, singleCourseLebel) => {
+        //     if (singleCourseLebel.course_label_no == value.course_label_no) {
+        //         courseSelect.append(`
+        //     <option selected value="${singleCourseLebel.course_label_no}">${singleCourseLebel.course_label_title}</option>
+        //     `);
+        //     } else {
+        //         courseSelect.append(`
+        // <option value="${singleCourseLebel.course_label_no}">${singleCourseLebel.course_label_title}</option>
+        // `);
+        //     }
+        // });
     });
 
+    $(`#addMenu`).on("click", function () {
+        selectParentMenuDisplayInModal();
+    });
+
+    function selectParentMenuDisplayInModal() {
+        let menus = pageSettingsFunc("menuData");
+        console.log(menus);
+        let select = $(`#parentmenu`);
+        let nullParent = menus.filter((menu) => menu.parentMenuTitle == null);
+        console.log(nullParent);
+        $.each(nullParent, (index, value) => {
+            select.append(`
+            <option value="${value.menuNo}">${value.menuName}</option>
+            `);
+        });
+    }
+
     // singleSave();
+}
+
+function showParentMenu(data) {
+    // let menus = pageSettingsFunc("menuData");
+    console.log(data, "here");
+
+    let elseParentOptions = data.filter(
+        (menu) =>
+            // menu.parentMenuTitle != data.parentMenuTitle &&
+            menu.parentMenuTitle == null
+    );
+    console.log(elseParentOptions);
+    $.each(elseParentOptions, (index, value) => {
+        $(`#parentmenu`).append(`
+        <option value="${value.parentMenuNo}">${value.menuName}</option>
+        `);
+    });
+    console.log(elseParentOptions);
+    $(`#makepage`).append(`<option selected>${data.makePageTitle}</option>`);
+    $(`#forlowernav`).append(
+        `<option selected>${data.forLowerNavTitle}</option>`
+    );
+    $(`#isexternallink`).append(
+        `<option selected>${data.isExternalLinkTitle}</option>`
+    );
+    $(`#menuname`).val(data.menuName);
+    $(`#menuSubmit`).text("Update Menu");
 }
--- MenuMapTable/index.php
+++ MenuMapTable/index.php
@@ -10,21 +10,15 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS, then Font Awesome -->
     <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
     <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/b9e84834a9.js" crossorigin="anonymous"></script>
     <style>
-        table,
-        th,
-        td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-
         /* setting the text-align property to center*/
-        th,
+
         td {
             padding: 5px;
             text-align: center;
@@ -37,57 +31,19 @@
 <body>
     <div class="container-fluid my-5">
         <div class="card">
-            <div class="card-body">
+            <div class="card-header">
                 <h3>Menu Map Table</h3>
             </div>
             <div class="card-body">
-                <!-- <form id="">
-                    <div class="border p-4">
-                        <div class="row">
-
-                            <div class="col p-2">
-                                <label for="basic-url">Course Level</label>
-                                <div class="input-group">
-                                    <select name="course_label" class="custom-select" id="courseLevelSelect">
-                                        <option selected>Select...</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col p-2">
-                                <label for="basic-url">Group</label>
-                                <div class="input-group">
-                                    <select name="group" class="custom-select" id="groupSelect">
-                                        <option selected>Select...</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col p-2">
-                                <label for="basic-url">Academic Year</label>
-                                <div class="input-group">
-                                    <input name="academic_year" type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
-                                </div>
-                            </div>
-
-                        </div>
-                        <div class="row d-flex justify-content-end p-2">
-                            <div class="">
-                                <button class="btn btn-primary btn-sm" type="submit">Filter</button>
-                                <button class="btn btn-secondary btn-sm">Save old to log</button>
-                                <button class="btn btn-warning btn-sm">Reset</button>
-                            </div>
-                        </div>
-
-                    </div>
-                </form> -->
                 <div class="my-3 d-flex justify-content-end">
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Add</button>
+                    <button type="button" class="btn btn-primary" id="addMenu" data-toggle="modal" data-target="#exampleModalCenter">Add</button>
                 </div>
-                <div class="">
-                    <table class="table table-bordered">
+                <div class="table-responsive">
+                    <table class="table table-sm table-bordered table-hover table-striped">
                         <thead>
-                            <tr>
-                                <th>Menu Name</th>
-                                <th>Page URL</th>
+                            <tr class="text-center">
+                                <th class="text-left">Menu Name</th>
+                                <th class="text-left">Page URL</th>
                                 <th>Parent Menu No</th>
                                 <th>Make Page</th>
                                 <th>For Lower Nav</th>
@@ -107,10 +63,10 @@
 
 
     <!-- Modal -->
-    <div class="modal fade bd-example-modal-lg" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-dialog-lg" role="document">
             <div class="modal-content">
-                <form id="menuForm">
+                <form id="menu_map_setup_form">
                     <div class="modal-header">
                         <h5 class="modal-title">Add Menu Map</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -145,12 +101,10 @@
                             <div class="row mb-3">
                                 <div class="col-sm">
                                     <div class="input-group-prepend">
-                                        <label for="parentmenuno">Parent Menu No</label>
+                                        <label for="parentmenu">Parent Menu</label>
                                     </div>
-                                    <select name="parentmenuno" class="custom-select" id="parentmenuno">
+                                    <select name="parentmenu" class="custom-select" id="parentmenu">
                                         <option selected>Choose...</option>
-                                        <option value="1">Yes</option>
-                                        <option value="2">No</option>
 
                                     </select>
                                 </div>
@@ -194,8 +148,8 @@
                                     </div>
                                     <select name="isexternallink" class="custom-select" id="isexternallink">
                                         <option selected>Choose...</option>
-                                        <option value="0">Not City</option>
-                                        <option value="1">City</option>
+                                        <option value="1">Yes</option>
+                                        <option value="2">No</option>
                                     </select>
                                 </div>
                             </div>
@@ -203,7 +157,7 @@
                     </div>
                     <div class="modal-footer">
                         <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
-                        <button type="submit" class="btn btn-primary">Make Menu</button>
+                        <button type="submit" id="menuSubmit" class="btn btn-primary">Make Menu</button>
                     </div>
                 </form>
             </div>
